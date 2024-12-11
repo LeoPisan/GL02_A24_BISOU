88ed30bd465d4feb48bc1b766b67d7e3048709d0
NathanHonoré <nathan.honore@outlook.fr>
2024-12-11 18:32:10 +0100

    faut juste réussir à extraire les données du parse

--- app/view/Main.js
+++ app/view/Main.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const GiftParser = require('../model/utils/giftParser');
 const addQuestionCommands = require('./QuestionCommands');
 const addTestCommands = require('./TestCommands');
-const TestController = require('../controller/TestController');
-const QuestionController = require('../controller/QuestionController');
+
 program
     .name("SRU Tests Manager by Bisou")
     .description("A brand new software for managing tests and questions at Sealand's Republic University")
@@ -21,9 +20,20 @@ program
         const data = fs.readFileSync(file, 'utf8');
         const parser = new GiftParser(true, true);
         parser.parse(data);
+        console.log('Parsed Questions:', parser.parsedQuestions);
 
-        const questions = parser.parsedQuestions;
+        /*const questions = parser.parsedQuestions;
         console.log('Parsed Questions:', questions);
+
+        // Add questions to your data
+        questions.forEach(question => {
+            // Assuming you have a function addQuestion to add questions to your data
+            addQuestion(question);
+        });
+
+        // Create a test using the extracted questions
+        const test = createTest(questions);
+        console.log('Created Test:', test);*/
     });
 
 program.parse(process.argv);
